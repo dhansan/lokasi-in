e6e0ecd24ef50c4060acc75d3af97f0855be648b
Dhanu Satriyo <dhanu.satriyo@gmail.com>
2018-05-17 13:19:19 +0700

    add gulp POST CSS and autoprefixer

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//////////////
+// Required //
+//////////////
+
+var gulp 		     = require('gulp'),
+    watch        = require('gulp-watch'),
+    postcss      = require('gulp-postcss'),
+    prefix       = require('autoprefixer'),
+    cssvars      = require('postcss-simple-vars'),
+    nested       = require('postcss-nested'),
+    cssImport    = require('postcss-import');
+
+
+
+//////////////////
+// Default Task //
+//////////////////
+gulp.task('default', function(){
+  console.log("Test berhasil");
+});
+
+gulp.task('html', function(){
+  console.log("HTML");
+});
+
+//////////////////
+// Styles  Task //
+//////////////////
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, prefix]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch',function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+});
