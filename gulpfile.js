820a7fdea90b57af0fb9766bbba312e97b405953
Dhanu Satriyo <dhanu.satriyo@gmail.com>
2018-05-18 22:04:33 +0700

    modified and sparated tasks styles.js and watch.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,6 @@
-//////////////
-// Required //
-//////////////
+/////////////////////
+// Import Required //
+/////////////////////
 
-var gulp 		     = require('gulp'),
-    watch        = require('gulp-watch'),
-    postcss      = require('gulp-postcss'),
-    prefix       = require('autoprefixer'),
-    cssvars      = require('postcss-simple-vars'),
-    nested       = require('postcss-nested'),
-    cssImport    = require('postcss-import'),
-    browserSync   = require ('browser-sync').create();
-
-
-
-//////////////////
-// Default Task //
-//////////////////
-gulp.task('default', function(){
-  console.log("Test berhasil");
-});
-
-gulp.task('html', function(){
-  console.log("HTML");
-});
-
-//////////////////
-// Styles  Task //
-//////////////////
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, prefix]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch',function(){
-
-  browserSync.init({
-    notify: false, 
-    server: {
-      baseDir: "app"
-    }
-  });
-  watch('./app/index.html', function(){
-    browserSync.reload();
-  });
-
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('cssInject');
-  });
-});
-
-gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
