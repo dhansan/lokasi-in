2e6adff2672c988b3e7b403e9c62907453b6eb41
Dhanu Satriyo <dhanu.satriyo@gmail.com>
2018-05-18 22:14:34 +0700

    add postcss-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -6,14 +6,15 @@ var gulp 		     = require('gulp'),
     prefix       = require('autoprefixer'),
     cssvars      = require('postcss-simple-vars'),
     nested       = require('postcss-nested'),
-    cssImport    = require('postcss-import');
+    cssImport    = require('postcss-import'),
+    mixins       = require('postcss-mixins');
 
 //////////////////
 // Styles  Task //
 //////////////////
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, prefix]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, prefix]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
